1f8741df4d132efb6c1e09389d911c1cdfab0973
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-09-14 16:10:28 -0300

    primeiro app react native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,17 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello world</Text>
+      <Message>Meu primeiro app com React Native</Message>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface MessageProps{
+  children: string
+}
+
+function Message({ children }: MessageProps){
+  return(
+    <Text style={styles.message}>
+      {children}
+    </Text>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 26,
+    fontWeight: 'bold'
+  },
+  message: {
+    color: '#fff'
+  }
 });
