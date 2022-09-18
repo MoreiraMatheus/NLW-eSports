770e5fd174d3cb7ab6128c0d8ac4ae76ba1d38b1
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-09-18 18:20:41 -0300

    Adicionando games no app mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,10 +6,10 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter'
-
-import { Home } from './src/screens/Home'; 
+ 
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded ? <Home/> : <Loading/>}
+      { fontsLoaded ? <Routes/> : <Loading/>}
     </Background>
   );
 }
